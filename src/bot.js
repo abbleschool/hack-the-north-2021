090906fb0f9ef1cf7adcde1eb6ca4428d91a5d31
abblesdabbles <78819595+abblesdabbles@users.noreply.github.com>
2021-09-18 03:10:02 -0400

    fdsafgsaga

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,11 @@
 require('dotenv').config();
 
-console.log(process.env.DISCORDJS_BOT_TOKEN);
+const { Client } = require('discord.js');
+const client = new Client(); //creating an instance of class
+
+
+client.login(process.env.DISCORDJS_BOT_TOKEN)//pass token to login
+//use variable bc credentials security
+
+
+//console.log(process.env.DISCORDJS_BOT_TOKEN);
